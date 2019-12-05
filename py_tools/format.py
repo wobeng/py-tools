46dad04a83473f48669e499c510b3c248d528cd5
Welby Obeng <wobeng@yblew.com>
2019-12-05 17:07:38 -0500

    refactored --skip-build

--- py_tools/format.py
+++ py_tools/format.py
@@ -2,6 +2,14 @@ import operator
 from functools import reduce
 
 
+def clean_empty(d):
+    if not isinstance(d, (dict, list)):
+        return d
+    if isinstance(d, list):
+        return [v for v in (clean_empty(v) for v in d) if v]
+    return {k: v for k, v in ((k, clean_empty(v)) for k, v in d.items()) if v != ''}
+
+
 class FormatData:
     def __init__(self, item):
         self.output = {}
