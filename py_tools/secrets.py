6d2bd27788b35937efe2fa323266d19dbeb21d8e
yblew <wobeng@yblew.com>
2020-04-05 22:53:56 -0400

    refactored

--- py_tools/secrets.py
+++ py_tools/secrets.py
@@ -4,19 +4,36 @@ import boto3
 
 from py_tools import format
 
+ssm = boto3.client('ssm')
 secretsmanager = boto3.client('secretsmanager')
 
 
-def load_secret(secrets):
-    if isinstance(secrets, str):
-        secrets = format.loads(secrets)
-    secrets = secrets or {}
-    for key, value in secrets.items():
-        if key not in os.environ:
-            os.environ[key] = value
-    return secrets
+def get_parameters(path=None, names=None, load=False):
+    if names:
+        response = ssm.get_parameters(
+            Names=names,
+            WithDecryption=True
+        )
+    else:
+        response = ssm.get_parameters_by_path(
+            Path=path,
+            WithDecryption=True
+        )
+    output = {}
+    for parameter in response['Parameters']:
+        output[parameter['Name'].split('/')[-1]] = parameter['Value']
+    if load:
+        print('Loading parameters...')
+        for k, v in output.items():
+            os.environ[k] = v
+    return output
 
 
 def load_secret_manager(secret_name):
     print('Loading secrets...')
-    return load_secret(secretsmanager.get_secret_value(SecretId=secret_name)['SecretString'])
+    secrets = secretsmanager.get_secret_value(SecretId=secret_name)['SecretString']
+    secrets = format.loads(secrets)
+    for key, value in secrets.items():
+        if key not in os.environ:
+            os.environ[key] = value
+    return secrets
