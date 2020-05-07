fff48eb314ce77eb7043cd0aa7164bd7c8074594
yblew <wobeng@yblew.com>
2020-05-07 15:55:19 -0400

    testing new environment.py

--- py_tools/secrets.py
+++ py_tools/secrets.py
@@ -8,6 +8,14 @@ ssm = boto3.client('ssm')
 secretsmanager = boto3.client('secretsmanager')
 
 
+def replace_value(value):
+    if value == 'true':
+        return True
+    elif value == 'false':
+        return False
+    return value
+
+
 def get_parameters(path=None, names=None, load=False):
     if names:
         response = ssm.get_parameters(
@@ -23,17 +31,22 @@ def get_parameters(path=None, names=None, load=False):
     for parameter in response['Parameters']:
         output[parameter['Name'].split('/')[-1]] = parameter['Value']
     if load:
-        print('Loading parameters...')
+        print('Adding parameters into env...')
         for k, v in output.items():
             os.environ[k] = v
+    output = {k: replace_value(v) for k, v in output.items()}
     return output
 
 
-def load_secret_manager(secret_name):
-    print('Loading secrets...')
+def load_secret_manager(secret_name, names=None, load=True):
     secrets = secretsmanager.get_secret_value(SecretId=secret_name)['SecretString']
     secrets = format.loads(secrets)
-    for key, value in secrets.items():
-        if key not in os.environ:
-            os.environ[key] = value
+    if names:
+        secrets = {k: v for k, v in secrets.items() if k in names}
+    if load:
+        print('Adding secrets into env...')
+        for key, value in secrets:
+            if key not in os.environ:
+                os.environ[key] = value
+    secrets = {k: replace_value(v) for k, v in secrets.items()}
     return secrets
