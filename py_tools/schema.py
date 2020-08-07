42ef29d1f22766bcee47d5d731b989d685f49a7f
yblew <wobeng@yblew.com>
2020-08-07 10:18:16 -0400

    refactored

--- py_tools/schema.py
+++ py_tools/schema.py
@@ -5,6 +5,6 @@ from py_tools import format
 
 def validate_schema_data(incoming_data, json_schema):
     incoming_data = format.clean_empty(incoming_data)
-    if not Draft7Validator(format.loads(json_schema)).is_valid(incoming_data):
+    if not Draft7Validator(json_schema).is_valid(incoming_data):
         raise BaseException
     return {'content': incoming_data, 'schema': json_schema}
