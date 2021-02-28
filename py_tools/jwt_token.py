5b7f77bbdba25a33452eb155a34bcde67ece39ce
welbyobeng <wobeng@yblew.com>
2021-02-27 19:21:03 -0500

    print error

--- py_tools/jwt_token.py
+++ py_tools/jwt_token.py
@@ -2,11 +2,11 @@ import jwt
 
 
 def encode_token(payload, secret=''):
-    output = jwt.encode(payload, secret)
+    output = jwt.encode(payload, secret, algorithm='HS256')
     if isinstance(output, str):
         return output
     return output.decode()
 
 
 def decode_token(payload, secret=''):
-    return jwt.decode(payload, secret)
+    return jwt.decode(payload, secret, algorithms=['HS256'])
