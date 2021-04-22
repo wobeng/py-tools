a9e4d81cc82f5226f2b14f2afda6403d50c031ff
Welby Obeng <wobeng@yblew.com>
2021-04-22 12:33:17 -0400

    working on high level working_on_highlevel_tasks

---
+++ py_tools/dydb_utils.py
@@ -0,0 +1,35 @@
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
+
+
+class StreamRecord:
+
+    @staticmethod
+    def deserialize_output(value):
+        try:
+            td = TypeDeserializer()
+            for k, v in dict(value).items():
+                value[k] = td.deserialize(v)
+        except BaseException:
+            pass
+        return value
+
+    @staticmethod
+    def serialize_output(value):
+        try:
+            td = TypeSerializer()
+            for k, v in dict(value).items():
+                value[k] = td.serialize(v)
+        except BaseException:
+            pass
+        return value
+
+    def __init__(self, record):
+        self.key = self.deserialize_output(record['dynamodb']['Keys'])
+        self.new_image = self.deserialize_output(record['dynamodb'].get('NewImage', {}))
+        self.old_image = self.deserialize_output(record['dynamodb'].get('OldImage', {}))
+        self.event_name = record['eventName'].lower()
+        self.table_name = record['eventSourceARN'].split('/')[-3]
+        if record.get('userIdentity', {}).get('type', '') == 'Service':
+            self.ttl = True
+        else:
+            self.ttl = False
