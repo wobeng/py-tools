de5154b0f0623100eabee0e747bbc6722b4ec6f9
Welby O <wobeng@users.noreply.github.com>
2022-04-02 14:34:45 +0000

    added put events

--- py_tools/cloudwatch.py
+++ py_tools/cloudwatch.py
@@ -1,4 +1,5 @@
 import boto3
+import time
 
 client = boto3.client("logs")
 
@@ -18,3 +19,16 @@ def get_events(log_group, log_stream):
         except KeyError:
             break
     return logs
+
+
+def put_events(log_group, log_stream, messages):
+    timestamp = int(round(time.time() * 1000))
+    client.create_log_stream(
+        logGroupName=log_group,
+        logStreamName=log_stream,
+    )
+    client.put_log_events(
+        logGroupName=log_group,
+        logStreamName=log_stream,
+        logEvents=[{"timestamp": timestamp, "message": m} for m in messages],
+    )
