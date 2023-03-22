65d0b85a325bd9f45b8e18478f7fbb5c57bc108f
welby <welby@caseactive.com>
2023-03-22 19:10:56 +0000

    refactor

--- py_tools/cmd.py
+++ py_tools/cmd.py
@@ -3,11 +3,11 @@ import subprocess
 import sys
 
 def execute_call(command, quiet=True, quiet_err=False, exit_script=True):
-    kwargs = {'shell': True}
+    kwargs = {"shell": True}
     if quiet:
-        kwargs['stdout'] = subprocess.DEVNULL
+        kwargs["stdout"] = subprocess.DEVNULL
     if quiet_err:
-        kwargs['stderr'] = subprocess.DEVNULL
+        kwargs["stderr"] = subprocess.DEVNULL
     code = subprocess.call(command, **kwargs)
     if code != 0 and exit_script:
         sys.exit(int(code))
@@ -15,11 +15,11 @@ def execute_call(command, quiet=True, quiet_err=False, exit_script=True):
 
 
 def execute_output(command, quiet=False, quiet_err=False, output_json=True):
-    kwargs = {'shell': True}
+    kwargs = {"shell": True}
     if quiet:
-        kwargs['stdout'] = subprocess.DEVNULL
+        kwargs["stdout"] = subprocess.DEVNULL
     if quiet_err:
-        kwargs['stderr'] = subprocess.DEVNULL
+        kwargs["stderr"] = subprocess.DEVNULL
     output = subprocess.check_output(command, **kwargs)
 
     if not output_json:
