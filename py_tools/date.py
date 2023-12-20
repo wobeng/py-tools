86bde2b0ec0185f2a5b507da591bcb66c637c5dc
welby <welby@caseactive.com>
2023-12-20 11:09:41 +0000

    updated id generator

--- py_tools/date.py
+++ py_tools/date.py
@@ -10,14 +10,17 @@ def datetime_utc(dt=None):
     return dt.replace(tzinfo=UTC)
 
 
+def id_generator(pre=""):
+    return pre + str(uuid.uuid4()).replace("-", "")[-14:]
+
+
 def date_id(prefix="", sep="::"):
     def call(pre=prefix):
-        uid = str(uuid.uuid4()).replace("-", "")
         # Getting the current time and formatting it without microseconds
         current_time = datetime.now().replace(tzinfo=None, microsecond=0)
         # Converting to ISO 8601 format without special characters
         formatted_time = current_time.isoformat().replace("-", "").replace(":", "").replace("T", "").replace("Z", "")
-        suf = formatted_time + "_" + uid[-14:]
+        suf = formatted_time + "_" +  id_generator()
         if pre:
             pre = pre + sep
         return pre + suf
