2ebfb5c41888499fddf0ef5d0be9aecf69363467
wobeng <wobeng@yblew.com>
2024-01-06 17:11:34 +0000

    refactor

--- py_tools/cmd.py
+++ py_tools/cmd.py
@@ -1,26 +1,41 @@
 import json
 import subprocess
 import sys
+import logging
 
-def execute_call(command, quiet=True, quiet_err=False, exit_script=True):
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def execute_call(command, quiet=True, quiet_err=False, exit_script=True, log_command=False):
     kwargs = {"shell": True}
     if quiet:
         kwargs["stdout"] = subprocess.DEVNULL
     if quiet_err:
         kwargs["stderr"] = subprocess.DEVNULL
+    if log_command:
+        logger.info("command: %s" % command)
+
     code = subprocess.call(command, **kwargs)
+
     if code != 0 and exit_script:
+        logger.info("command: %s" % command)
         sys.exit(int(code))
+
     return code
 
 
-def execute_output(command, quiet=False, quiet_err=False, output_json=True):
+def execute_output(command, quiet=False, quiet_err=False, output_json=True, log_command=False):
     kwargs = {"shell": True}
     if quiet:
         kwargs["stdout"] = subprocess.DEVNULL
     if quiet_err:
         kwargs["stderr"] = subprocess.DEVNULL
+    if log_command:
+        logger.info("command: %s" % command)
+
     output = subprocess.check_output(command, **kwargs)
+    output = output.decode("utf-8")  # Explicitly decode using UTF-8
 
     if not output_json:
         return output
@@ -29,4 +44,5 @@ def execute_output(command, quiet=False, quiet_err=False, output_json=True):
         output = json.loads(output)
     except json.decoder.JSONDecodeError:
         pass
+
     return output
