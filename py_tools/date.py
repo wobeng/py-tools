0f65cdd2f7bd83913dd1e5c179967e5300b1c628
welby <welby@caseactive.com>
2024-03-23 19:00:27 +0000

    refactoring date

--- py_tools/date.py
+++ py_tools/date.py
@@ -1,19 +1,19 @@
 import uuid
-from datetime import datetime
-
-from pytz import UTC
+from datetime import datetime, timezone
 
 
 def datetime_utc(dt=None):
     if not dt:
-        dt = datetime.utcnow()
-    return dt.replace(tzinfo=UTC)
+        dt = datetime.now(timezone.utc)
+    return dt.replace(tzinfo=timezone.utc)
 
 
 def id_generator(prefix=""):
     def wrapper(pre=prefix):
         return pre + str(uuid.uuid4()).replace("-", "")[-14:]
+
     return wrapper
+
 
 def formatted_time(datetime_obj=None):
     # Getting the current time and formatting it without microseconds
@@ -21,10 +21,11 @@ def formatted_time(datetime_obj=None):
     # Converting to ISO 8601 format without special characters
     return datetime_obj.strftime("%Y%m%d%H%M%S")
 
+
 def date_id(prefix="", sep="::"):
     def wrapper(pre=prefix):
         f_time = formatted_time()
-        suf = f_time + "_" +  id_generator()()
+        suf = f_time + "_" + id_generator()()
         if pre:
             pre = pre + sep
         return pre + suf
