a752336789935410521ec68632d32244c11fb411
welby <welby@caseactive.com>
2024-11-12 20:12:07 +0000

    update

--- py_tools/secrets.py
+++ py_tools/secrets.py
@@ -9,10 +9,11 @@ ssm = boto3.client("ssm")
 
 
 def environ_wrap(value):
-    if value.lower() == "true":
-        return "true"
-    if value.lower() == "false":
-        return "false"
+    if isinstance(value, str):
+        if value.lower() == "true":
+            return "true"
+        if value.lower() == "false":
+            return "false"
     if isinstance(value, bool):
         return str(value).lower()
     return value
